4e416d0a0c43e95861d694008d90c0e2201aecef
ajimaruu <ajiseno360@gmail.com>
2026-03-31 08:22:00 +0700

    Update Week 3

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,39 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center text-center">
+      <div className='flex items-center justify-center space-x-8 mb-12'>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} 
+          className="logo w-24 h-24 animate-spin" 
+          alt="Vite logo" 
+          style={{animationDuration: '10s'}}
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} 
+          className="logo react w-24 h-24 animate-spin" 
+          alt="React logo" 
+          style={{animationDuration: '10s'}}
+          />
         </a>
       </div>
-      <h1 className="bg-blue-200"> vite + react </h1>
-      <h3>Aji Bayu Seno</h3>
-        <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className='text-white text-6xl font-bold mb-5'> vite + react </h1>
+      <h3 className='text-white text-2xl mb-10'>Aji Bayu Seno</h3>
+        <div className="text-sm space-y-6 mb-8">
+        <button 
+        onClick={() => setCount((count) => count + 1)} 
+        className="bg-neutral-500 text-white py-2 px-6 rounded-lg text-lg">
           count is {count}
         </button>
-        <p>
+        <p className='text-neutral-300'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-neutral-400">
         Click on the Vite and React logos to learn more
       </p>
+      </div>
     </>
   )
 }
