329dca17571f84bd69b169cd5b7d535ecde3ce5f
ajimaruu <ajiseno360@gmail.com>
2026-04-21 00:46:33 +0700

    membuat user Card

---
+++ src/UserCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function UserCard(props) {
+const { name, email, street, city } = props;
+const [isClicked, setIsClicked] = React.useState(false);
+
+   return (
+    <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
+      <p className="text-gray-600">
+        <span className="font-medium">Email:</span> {email}
+      </p>
+      <p className="text-gray-600">
+        <span className="font-medium">Address:</span> {street}, {city}
+      </p>
+      <button className={isClicked ? 'bg-special-green text-white px-4 py-2 rounded md' : 'bg-gray-01 text-white px-4 py-2 rounded md'}
+      onClick={() => setIsClicked(true)}>
+      {isClicked ? 'Clicked!' : 'Click Me'}
+      </button>
+    </div>
+  );
+}
+
+export default UserCard
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App';
 import './index.css'
-import App from './App.jsx'
+import Exercise from './exercise';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Exercise/>
   </StrictMode>,
 )
