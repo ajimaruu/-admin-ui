6dec509edced5722b55d4e0d11f05060c128d047
ajimaruu <ajiseno360@gmail.com>
2026-04-21 08:25:20 +0700

    membuat user Card2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import './index.css'
-import Exercise from './exercise';
+import Exercise from './Exercise';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
